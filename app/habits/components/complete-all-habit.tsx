59a14f1d7be354945355022c6a56e994bd78c432
fiqriardiansyah <fiqriardian92@gmail.com>
2024-08-29 10:38:59 +0700

    fix: CompleteAllHabit component

--- app/habits/components/complete-all-habit.tsx
+++ app/habits/components/complete-all-habit.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Lottie from "react-lottie";
 
 const defaultOptions = {
@@ -15,9 +16,21 @@ export type CompleteAllHabitProps = {
 }
 
 export default function CompleteAllHabit({ anim }: CompleteAllHabitProps) {
+    const [animJson] = React.useState(() => anim);
+
     return (
-        <div className="rounded-xl ">
-            <Lottie style={{ pointerEvents: 'none' }} options={{ ...defaultOptions, animationData: anim }} height={100} width={100} />
+        <div className="rounded-xl flex gap-3 relative p-2 overflow-hidden">
+            <Lottie style={{ pointerEvents: 'none', zIndex: 1 }} options={{ ...defaultOptions, animationData: animJson }} height={100} width={100} />
+            <div className="absolute top-1/2 transform -translate-y-1/2 -right-1/4 z-[1] opacity-30 blur-sm">
+                <Lottie
+                    style={{ pointerEvents: 'none' }}
+                    options={{ ...defaultOptions, loop: false, animationData: animJson }}
+                    height={300} width={300} />
+            </div>
+            <div className="flex flex-col gap-2 flex-1 z-10">
+                <h1 className="text-xl font-medium">Todays Habit is <br /> Complete 👍</h1>
+                <button className="border-none bg-transparent cursor-pointer text-green-400 text-xs w-fit">See history</button>
+            </div>
         </div>
     )
 }
