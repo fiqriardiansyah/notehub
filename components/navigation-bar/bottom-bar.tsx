f419d0f3f99c50aea06eacd04896a350cfc00290
fiqriardiansyah <fiqriardian92@gmail.com>
2024-09-08 00:21:22 +0700

    feat: add count habit on bottom nav habit button

--- components/navigation-bar/bottom-bar.tsx
+++ components/navigation-bar/bottom-bar.tsx
@@ -5,9 +5,15 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import themeColor from 'tailwindcss/colors';
 import { motion } from "framer-motion";
+import habitsService from "@/service/habits";
+import { useQuery } from "@tanstack/react-query";
 
 export default function BottomBar() {
     const pathname = usePathname();
+
+    const habitsToday = useQuery([habitsService.getUrgentHabit.name, "habitstoday"], async () => {
+        return (await habitsService.getUrgentHabit()).data.data;
+    });
 
     return (
         <motion.div animate={{ y: 0, transition: { delay: 0.8 } }} initial={{ y: '100%' }} className="fixed bottom-0 left-0 right-0 w-screen container-custom z-50 bg-white">
@@ -15,7 +21,14 @@ export default function BottomBar() {
                 <Link style={{ color: pathname === "/" ? themeColor.gray[700] : themeColor.gray[400] }} href="/"><House /></Link>
                 <Link style={{ color: themeColor.gray[400] }} href="#"><Shell /></Link>
                 <Link style={{ color: pathname.includes("/write") ? themeColor.gray[700] : themeColor.gray[400] }} href="/write"><Plus /></Link>
-                <Link style={{ color: pathname.includes("/habits") ? themeColor.gray[700] : themeColor.gray[400] }} href="/habits"><Trophy /></Link>
+                <Link style={{ color: pathname.includes("/habits") ? themeColor.gray[700] : themeColor.gray[400] }} className="relative" href="/habits">
+                    {!habitsToday.isLoading
+                        && habitsToday.data?.length
+                        && <span className="absolute -top-[10px] -right-[10px] w-5 h-5 rounded-full bg-yellow-500 text-white text-xs font-semibold flex items-center justify-center">
+                            {habitsToday.data?.length}
+                        </span>}
+                    <Trophy />
+                </Link>
                 <Link style={{ color: themeColor.gray[400] }} href="#"><PanelsTopLeft /></Link>
             </div>
         </motion.div>
