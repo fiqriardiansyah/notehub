e8c11109866ddbcc69683494281505414eb1d3c2
fiqriardiansyah <fiqriardian92@gmail.com>
2024-09-09 09:02:18 +0700

    fix: count habit in bottom bar

--- components/navigation-bar/bottom-bar.tsx
+++ components/navigation-bar/bottom-bar.tsx
@@ -22,11 +22,11 @@ export default function BottomBar() {
                 <Link style={{ color: themeColor.gray[400] }} href="#"><Shell /></Link>
                 <Link style={{ color: pathname.includes("/write") ? themeColor.gray[700] : themeColor.gray[400] }} href="/write"><Plus /></Link>
                 <Link style={{ color: pathname.includes("/habits") ? themeColor.gray[700] : themeColor.gray[400] }} className="relative" href="/habits">
-                    {!habitsToday.isLoading
-                        && habitsToday.data?.length
-                        && <span className="absolute -top-[10px] -right-[10px] w-5 h-5 rounded-full bg-yellow-500 text-white text-xs font-semibold flex items-center justify-center">
+                    {(!habitsToday.isLoading
+                        && habitsToday.data?.length)
+                        ? <span className="absolute -top-[10px] -right-[10px] w-5 h-5 rounded-full bg-yellow-500 text-white text-xs font-semibold flex items-center justify-center">
                             {habitsToday.data?.length}
-                        </span>}
+                        </span> : null}
                     <Trophy />
                 </Link>
                 <Link style={{ color: themeColor.gray[400] }} href="#"><PanelsTopLeft /></Link>
