e040e7ab4e276c5261647bb9c8900cf366e7022b
fiqriardiansyah <fiqriardian92@gmail.com>
2024-09-12 09:07:05 +0700

    fix: remove animation on habits

--- app/components/habits/habits-urgent.tsx
+++ app/components/habits/habits-urgent.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { Todo } from "@/app/write/mode/todolist";
+import Countdown from "@/components/common/countdown";
 import { Checkbox } from "@/components/ui/checkbox";
+import useHabitComplete, { JSON_ANIMATIONS } from "@/hooks/use-habit-complete";
 import useSkipFirstRender from "@/hooks/use-skip-first-render";
 import { easeDefault, hexToRgba, progressCheer } from "@/lib/utils";
 import habitsService from "@/service/habits";
 import noteService from "@/service/note";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import { AnimatePresence, HTMLMotionProps, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Blocks, Check, Timer } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import HabitsCountdown from "./habits-countdown";
-import useHabitComplete, { JSON_ANIMATIONS } from "@/hooks/use-habit-complete";
-import HabitsTimer from "./habits-timer";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
-import Countdown from "@/components/common/countdown";
 import themeColor from "tailwindcss/colors";
+import HabitsCountdown from "./habits-countdown";
+import HabitsTimer from "./habits-timer";
 
-export type HabitsUrgentProps = HTMLMotionProps<"div"> & {
+export type HabitsUrgentProps = React.HTMLProps<HTMLDivElement> & {
     onChangeHabit?: () => void;
     inPageHabits?: boolean;
     renderWhenComplete?: (randomAnim: any) => any;
@@ -155,121 +155,117 @@ export default function HabitsUrgent({ onChangeHabit, renderWhenComplete, inPage
             {habit && <div className="flex w-full justify-end">
                 <Link href={`/habits/${habit?.id}`} passHref> <a className="text-gray-400 text-xs font-light">View Details</a> </Link>
             </div>}
-            <AnimatePresence>
-                {habit && (
-                    <motion.div
-                        className={`${className} rounded-md p-3 flex flex-col gap-3 w-full relative overflow-hidden`}
-                        key={habit?.id} exit={{ opacity: 0, height: 0 }} initial={{ height: 0, opacity: 0 }} animate={{ height: 'auto', opacity: 1 }}
-                        {...props} >
-                        <div className="font-semibold z-10 leading-none">
-                            {!inPageHabits && <p className="text-sm z-10 m-0">Habits on this <span className="text-xs font-light">{nameOftheDay()?.date}</span> </p>}
-                            {inPageHabits && <p className="text-sm z-10 m-0 line-clamp-1 flex flex-row">
-                                <Blocks className="mr-2 text-gray-500" size={18} />
-                                {habit?.title}
-                            </p>}
-                            <div className="overflow-y-hidden h-[36px] flex-1 relative">
-                                <AnimatePresence mode="wait">
-                                    {progressCheer.map((pc) => {
-                                        if (pc.donepoint === progressDoneCheer) {
-                                            return (
-                                                <motion.p
-                                                    key={pc.donepoint}
-                                                    initial={{ y: '40px' }}
-                                                    animate={{ y: 0 }}
-                                                    exit={{ y: '-40px' }}
-                                                    style={{ background: pc.bgColor }}
-                                                    className="font-semibold p-1 px-2 rounded w-fit text-xl text-white capitalize">
-                                                    {pc.content}
-                                                </motion.p>
-                                            )
-                                        }
-                                        return null
-                                    })}
-                                    {(progressDoneCheer === null || progressDoneCheer === undefined) &&
-                                        <motion.div
-                                            key="day"
+            <div
+                className={`${className} rounded-md p-3 flex flex-col gap-3 w-full relative overflow-hidden`}
+                key={habit?.id}
+                {...props} >
+                <div className="font-semibold z-10 leading-none">
+                    {!inPageHabits && <p className="text-sm z-10 m-0">Habits on this <span className="text-xs font-light">{nameOftheDay()?.date}</span> </p>}
+                    {inPageHabits && <p className="text-sm z-10 m-0 line-clamp-1 flex flex-row">
+                        <Blocks className="mr-2 text-gray-500" size={18} />
+                        {habit?.title}
+                    </p>}
+                    <div className="overflow-y-hidden h-[36px] flex-1 relative">
+                        <AnimatePresence mode="wait">
+                            {progressCheer.map((pc) => {
+                                if (pc.donepoint === progressDoneCheer) {
+                                    return (
+                                        <motion.p
+                                            key={pc.donepoint}
                                             initial={{ y: '40px' }}
                                             animate={{ y: 0 }}
                                             exit={{ y: '-40px' }}
-                                            className={`font-semibold text-xl capitalize flex items-center gap-4`}>
-                                            {nameOftheDay()?.name}
-                                            <AnimatePresence>
-                                                {isDone && <motion.button
-                                                    onClick={onClickDone}
-                                                    exit={{ opacity: 0 }}
-                                                    className="flex text-xs font-light items-center gap-2 bg-yellow-500 text-white border-none rounded-md px-2 py-1">
-                                                    Check as Done <Check />
-                                                </motion.button>}
-                                            </AnimatePresence>
-                                        </motion.div>
-                                    }
-                                </AnimatePresence>
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-2 p-1 w-full max-h-[250px] overflow-y-auto z-10">
-                            {todos.map((todo) => (
-                                <div key={todo.id} className="w-full flex items-center justify-between">
-                                    <div className="flex gap-2 items-center h-[40px]">
-                                        <HabitsTimer setTimer={onSetTimer} todo={todo}>
-                                            {(ctrl) => {
-                                                if (todo?.timer && !todo?.timer?.isEnd) {
-                                                    return (
-                                                        <Countdown
-                                                            onCompleteRender={ctrl.isOpen ? () => { } : () => onCompleteTimer(todo)}
-                                                            endTime={todo?.timer?.endTime}
-                                                            startTime={todo?.timer?.startTime}>
-                                                            {({ text, progress }) => (
-                                                                <button onClick={todo?.isCheck ? () => { } : ctrl.open} className="w-10 h-10 rounded-full relative">
-                                                                    <CircularProgressbar text="" value={progress} styles={buildStyles({
-                                                                        textSize: '10px',
-                                                                        textColor: hexToRgba("#000000", 0.7),
-                                                                        backgroundColor: "#00000000",
-                                                                        pathColor: themeColor.gray[500],
-                                                                    })} />
-                                                                    <Timer size={16} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-                                                                </button>)}
-                                                        </Countdown>
-                                                    )
-                                                }
-                                                return (
-                                                    <button
-                                                        onClick={todo?.isCheck ? () => { } : ctrl.open}
-                                                        className={`w-10 h-10 rounded-full flex items-center justify-center ${todo?.isCheck ? "text-gray-400 pointer-events-none" : ""}`}>
-                                                        <Timer />
-                                                    </button>
-                                                )
-                                            }}
-                                        </HabitsTimer>
-                                        <div className="">
-                                            <p className="text-sm m-0 capitalize leading-none line-clamp-1">{todo.content}</p>
-                                            {todo.isCheck && (
-                                                <span className="text-xs font-light m-0 leading-none">done at {dayjs(todo.checkedAt).format("DD MMM, HH:mm")}</span>
-                                            )}
-                                        </div>
-                                    </div>
-                                    <Checkbox checked={todo.isCheck} onCheckedChange={onUpdateCheck(todo)} />
+                                            style={{ background: pc.bgColor }}
+                                            className="font-semibold p-1 px-2 rounded w-fit text-xl text-white capitalize">
+                                            {pc.content}
+                                        </motion.p>
+                                    )
+                                }
+                                return null
+                            })}
+                            {(progressDoneCheer === null || progressDoneCheer === undefined) &&
+                                <motion.div
+                                    key="day"
+                                    initial={{ y: '40px' }}
+                                    animate={{ y: 0 }}
+                                    exit={{ y: '-40px' }}
+                                    className={`font-semibold text-xl capitalize flex items-center gap-4`}>
+                                    {nameOftheDay()?.name}
+                                    <AnimatePresence>
+                                        {isDone && <motion.button
+                                            onClick={onClickDone}
+                                            exit={{ opacity: 0 }}
+                                            className="flex text-xs font-light items-center gap-2 bg-yellow-500 text-white border-none rounded-md px-2 py-1">
+                                            Check as Done <Check />
+                                        </motion.button>}
+                                    </AnimatePresence>
+                                </motion.div>
+                            }
+                        </AnimatePresence>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 p-1 w-full max-h-[250px] overflow-y-auto z-10">
+                    {todos.map((todo) => (
+                        <div key={todo.id} className="w-full flex items-center justify-between">
+                            <div className="flex gap-2 items-center h-[40px]">
+                                <HabitsTimer setTimer={onSetTimer} todo={todo}>
+                                    {(ctrl) => {
+                                        if (todo?.timer && !todo?.timer?.isEnd) {
+                                            return (
+                                                <Countdown
+                                                    onCompleteRender={ctrl.isOpen ? () => { } : () => onCompleteTimer(todo)}
+                                                    endTime={todo?.timer?.endTime}
+                                                    startTime={todo?.timer?.startTime}>
+                                                    {({ text, progress }) => (
+                                                        <button onClick={todo?.isCheck ? () => { } : ctrl.open} className="w-10 h-10 rounded-full relative">
+                                                            <CircularProgressbar text="" value={progress} styles={buildStyles({
+                                                                textSize: '10px',
+                                                                textColor: hexToRgba("#000000", 0.7),
+                                                                backgroundColor: "#00000000",
+                                                                pathColor: themeColor.gray[500],
+                                                            })} />
+                                                            <Timer size={16} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                                                        </button>)}
+                                                </Countdown>
+                                            )
+                                        }
+                                        return (
+                                            <button
+                                                onClick={todo?.isCheck ? () => { } : ctrl.open}
+                                                className={`w-10 h-10 rounded-full flex items-center justify-center ${todo?.isCheck ? "text-gray-400 pointer-events-none" : ""}`}>
+                                                <Timer />
+                                            </button>
+                                        )
+                                    }}
+                                </HabitsTimer>
+                                <div className="">
+                                    <p className="text-sm m-0 capitalize leading-none line-clamp-1">{todo.content}</p>
+                                    {todo.isCheck && (
+                                        <span className="text-xs font-light m-0 leading-none">done at {dayjs(todo.checkedAt).format("DD MMM, HH:mm")}</span>
+                                    )}
                                 </div>
-                            ))}
+                            </div>
+                            <Checkbox checked={todo.isCheck} onCheckedChange={onUpdateCheck(todo)} />
                         </div>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex flex-col justify-end">
-                            <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-                                viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-                                <defs>
-                                    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                                </defs>
-                                <g className="parallax">
-                                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(172 176 255)" />
-                                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="#c5c5ff" />
-                                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="#d3d3ff" />
-                                </g>
-                            </svg>
-                            <motion.div animate={{ height: `${progress}%` }}
-                                transition={{ ease: easeDefault, delay: 0.4, duration: 0.8 }} className="transition duration-200 bg-[#d3d3ff] w-full"></motion.div>
-                        </div>
-                    </motion.div>
-                )}
-                {!habit && renderWhenComplete && renderWhenComplete(JSON_ANIMATIONS[Math.floor(Math.random() * JSON_ANIMATIONS.length)])}
-            </AnimatePresence>
+                    ))}
+                </div>
+                <div className="absolute bottom-0 left-0 w-full h-full flex flex-col justify-end">
+                    <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                        viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+                        <defs>
+                            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                        </defs>
+                        <g className="parallax">
+                            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(172 176 255)" />
+                            <use xlinkHref="#gentle-wave" x="48" y="3" fill="#c5c5ff" />
+                            <use xlinkHref="#gentle-wave" x="48" y="5" fill="#d3d3ff" />
+                        </g>
+                    </svg>
+                    <motion.div animate={{ height: `${progress}%` }}
+                        transition={{ ease: easeDefault, delay: 0.4, duration: 0.8 }} className="transition duration-200 bg-[#d3d3ff] w-full"></motion.div>
+                </div>
+            </div>
+            {!habit && renderWhenComplete && renderWhenComplete(JSON_ANIMATIONS[Math.floor(Math.random() * JSON_ANIMATIONS.length)])}
             {habit?.id && <HabitsCountdown noteHabits={habit || undefined} />}
         </div>
     )
--- app/habits/page.tsx
+++ app/habits/page.tsx
@@ -1,45 +1,25 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { CalendarRange, ChevronLeft } from "lucide-react";
-import HabitsUrgent from "../components/habits/habits-urgent";
-import { useRouter } from "next-nprogress-bar";
-import habitsService from "@/service/habits";
-import { useQuery } from "@tanstack/react-query";
-import ListCardHabit from "./components/list-card-habit";
-import { AnimatePresence, motion } from "framer-motion";
-import CompleteAllHabit from "./components/complete-all-habit";
-import moment from "moment";
 import BottomBar from "@/components/navigation-bar/bottom-bar";
+import StateRender from "@/components/state-render";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import useToggleHideNav from "@/hooks/use-toggle-hide-nav";
 import { easeDefault } from "@/lib/utils";
+import habitsService from "@/service/habits";
+import { useQuery } from "@tanstack/react-query";
+import { motion } from "framer-motion";
+import { ChevronLeft } from "lucide-react";
+import moment from "moment";
+import { useRouter } from "next-nprogress-bar";
 import React from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import HabitsUrgent from "../components/habits/habits-urgent";
 import AllHabits from "./components/all-habits";
+import CompleteAllHabit from "./components/complete-all-habit";
 import DailyHabits from "./components/daily-habits";
-import WeeklyHabits from "./components/weekly-habits";
+import ListCardHabit from "./components/list-card-habit";
 import MonthlyHabits from "./components/monthly-habits";
-import StateRender from "@/components/state-render";
-import PickDate from "./components/pick-date";
-
-const staggerVariants = {
-    initial: { opacity: 0, x: '100%' },
-    animate: {
-        opacity: 1,
-        x: 0,
-        transition: {
-            staggerChildren: 0.2, // Delay between the animations of each child
-        },
-    },
-    exit: { opacity: 0, x: '-100%' },
-};
-
-const itemVariants = {
-    initial: { opacity: 0, x: '100%' },
-    animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, x: '-100%' },
-};
+import WeeklyHabits from "./components/weekly-habits";
 
 const tabs = [
     {
@@ -126,21 +106,11 @@ export default function Habits() {
                                     )
                                 }} />
                             {habitsToday.data?.length ? <p className="text-2xl my-3 mt-5">Should Do! 💪</p> : null}
-                            <AnimatePresence>
-                                <motion.div
-                                    variants={staggerVariants}
-                                    initial="initial"
-                                    animate="animate"
-                                    exit="exit"
-                                    className="flex flex-col gap-3"
-                                >
-                                    {habitsToday.data?.map((habit, i) => (
-                                        <motion.div key={habit.id} variants={itemVariants} >
-                                            <ListCardHabit index={i} habit={habit} />
-                                        </motion.div>
-                                    ))}
-                                </motion.div>
-                            </AnimatePresence>
+                            <div className="flex flex-col gap-3" >
+                                {habitsToday.data?.map((habit, i) => (
+                                    <ListCardHabit key={habit.id} index={i} habit={habit} />
+                                ))}
+                            </div>
                         </div>
                     ) : null}
                 </StateRender.Data>
