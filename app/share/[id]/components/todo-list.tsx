7df1bcddae46170f1e84c4bd52f8ff0d583f2151
fiqriardiansyah <fiqriardian92@gmail.com>
2024-10-02 13:31:42 +0700

    fix: change font size todolist view

--- app/share/[id]/components/todo-list.tsx
+++ app/share/[id]/components/todo-list.tsx
@@ -16,8 +16,8 @@ export default function TodoListView({ todos }: TodoListViewProps) {
                     <label htmlFor={item.id} className="flex items-center gap-3 p-1">
                         <Checkbox id={item.id} disabled checked={item.isCheck} className="w-7 h-7 !cursor-pointer" />
                         <div className="flex flex-col">
-                            <span className="text-lg font-medium">{item.content}</span>
-                            {item.checkedAt && <span className="text-sm font-medium text-gray-400 capitalize">done at {dayjs(item.checkedAt).format("DD MMM, HH:mm")}</span>}
+                            <span className="text-sm font-medium">{item.content}</span>
+                            {item.checkedAt && <span className="text-xs font-medium text-gray-400 capitalize">done at {dayjs(item.checkedAt).format("DD MMM, HH:mm")}</span>}
                         </div>
                     </label>
                 </div>
